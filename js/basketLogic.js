873f1ce749d0e950efde928ad5d9f6a5e8a2a89d
Harris Symeoudakis <harris.symeoudakis@gmail.com>
2024-06-04 09:52:26 +0300

    Update basketLogic.js

--- js/basketLogic.js
+++ js/basketLogic.js
@@ -165,8 +165,8 @@ async function getToken() {
     console.log("hello");
     return new Promise((resolve, reject) => {
         var tokenRequest = new XMLHttpRequest();
-        var tokenUrl = 'http://localhost:3000/et/as/connect/token'; // Proxy server URL
-        //var tokenUrl = 'https://proxyserver-z74x.onrender.com/et/as/connect/token'; // Proxy server URL
+        //var tokenUrl = 'http://localhost:3000/et/as/connect/token'; // Proxy server URL
+        var tokenUrl = 'https://proxyserver-z74x.onrender.com/et/as/connect/token'; // Proxy server URL
         var tokenData = 'client_id=CegidRetailResourceFlowClient&username=AI@90478305_003_TEST&password=1234&grant_type=password&scope=RetailBackendApi offline_access'; // Construct x-www-form-urlencoded body
 
         tokenRequest.open('POST', tokenUrl, true);
@@ -191,8 +191,8 @@ async function getToken() {
 
 document.getElementById('viewBasketAll').addEventListener('click', function() {
     var xhr = new XMLHttpRequest();
-    var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
-    //var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
+    //var postUrl = 'http://localhost:3000/et/pos/external-basket/v1'; // Proxy server URL
+    var postUrl = 'https://proxyserver-z74x.onrender.com/et/pos/external-basket/v1'; // Proxy server URL
     xhr.open('POST', postUrl, true);
     xhr.setRequestHeader('Content-Type', 'application/json');
     xhr.setRequestHeader('Authorization', 'Bearer ' + accessToken); // Include access token in the request headers
